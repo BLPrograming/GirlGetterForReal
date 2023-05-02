78ee19c01a10a868de00a7024b82944cefad7798
BLPrograming <bryan.landsnes@icloud.com>
2023-05-02 14:18:40 -0500

    Initial Commit

---
+++ GirlGetter/GirlGetterApp.swift
@@ -0,0 +1,17 @@
+//
+//  GirlGetterApp.swift
+//  GirlGetter
+//
+//  Created by Bryan C. Landsnes on 5/2/23.
+//
+
+import SwiftUI
+
+@main
+struct GirlGetterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
