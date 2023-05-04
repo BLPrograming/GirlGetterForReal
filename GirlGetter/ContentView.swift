1b216f05287ea56da0c7d39521ad3831ed1797d3
BLPrograming <bryan.landsnes@icloud.com>
2023-05-04 14:57:44 -0500

    latest version

--- GirlGetter/ContentView.swift
+++ GirlGetter/ContentView.swift
@@ -9,84 +9,82 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Text("Girl")
-                .foregroundColor(.blue)
-            
-            
-            Text("Getter")
-                .foregroundColor(Color(hue: 0.872, saturation: 0.985, brightness: 0.962))
+        NavigationView {
+            VStack {
+                Text("Girl")
+                    .foregroundColor(.blue)
+                
+                
+                Text("Getter")
+                    .foregroundColor(Color(hue: 0.872, saturation: 0.985, brightness: 0.962))
+                
+                    .padding(.bottom)
+                    .padding(.bottom)
+                    .padding(.bottom)
+                    .padding(.bottom)
+                
+                NavigationLink(destination: SignUpGirl()) {
+                    Text("Sign Up As A Girl")
+                        .frame(minWidth: 0, maxWidth: 150)
+                        .font(.system(size: 18))
+                        .padding()
+                        .foregroundColor(.black)
+                }
+                .border(Color(hue: 0.872, saturation: 0.985, brightness: 0.962))
                 
                 .padding(.bottom)
                 .padding(.bottom)
+                
+                
+                NavigationLink(destination: SignUpGirl()) {
+                    Text("Sign Up As A Boy")
+                        .frame(minWidth: 0, maxWidth: 150)
+                        .font(.system(size: 18))
+                        .padding()
+                        .foregroundColor(.black)
+                }
+                .border(Color.blue)
+                
                 .padding(.bottom)
                 .padding(.bottom)
-            
-            Button(action: {
-                print("signed up as a girl")
-            }) {
-                Text("Sign Up As A Girl")
-                    .frame(minWidth: 0, maxWidth: 150)
-                    .font(.system(size: 18))
-                    .padding()
-                    .foregroundColor(.black)
+                
+                NavigationLink(destination: SignUpGirl()) {
+                    Text("Log In As A Girl")
+                        .frame(minWidth: 0, maxWidth: 150)
+                        .font(.system(size: 18))
+                        .padding()
+                        .foregroundColor(.black)
+                }
+                .border(Color(hue: 0.872, saturation: 0.985, brightness: 0.962))
+                
+                .padding(.bottom)
+                .padding(.bottom)
+                
+                NavigationLink(destination: SignUpGirl()) {
+                    Text("Log In As A Boy")
+                        .frame(minWidth: 0, maxWidth: 150)
+                        .font(.system(size: 18))
+                        .padding()
+                        .foregroundColor(.black)
+                }
+                .border(Color.blue)
+                
+                .padding(.bottom)
+                .padding(.bottom)
+                
+                
             }
-            .border(Color(hue: 0.872, saturation: 0.985, brightness: 0.962))
-            
             .padding(.bottom)
             .padding(.bottom)
-            
-            
-            Button(action: {
-                print("signed up as a boy")
-            }) {
-                Text("Sign Up As A Boy")
-                    .frame(minWidth: 0, maxWidth: 150)
-                    .font(.system(size: 18))
-                    .padding()
-                    .foregroundColor(.black)
-            }
-            .border(Color.blue)
             .padding(.bottom)
             .padding(.bottom)
-            
-            Button(action: {
-                print("logged in as a girl")
-            }) {
-                Text("Log In As A Girl")
-                    .frame(minWidth: 0, maxWidth: 150)
-                    .font(.system(size: 18))
-                    .padding()
-                    .foregroundColor(.black)
-            }
-            .border(Color(hue: 0.872, saturation: 0.985, brightness: 0.962))
             .padding(.bottom)
             .padding(.bottom)
-            
-            Button(action: {
-                print("logged in as a boy")
-            }) {
-                Text("Log In As A Boy")
-                    .frame(minWidth: 0, maxWidth: 150)
-                    .font(.system(size: 18))
-                    .padding()
-                    .foregroundColor(.black)
-            }
-            .border(Color.blue)
-            
-            
+            .padding(.bottom)
+            .padding(.bottom)
         }
-        .padding(.bottom)
-        .padding(.bottom)
-        .padding(.bottom)
-        .padding(.bottom)
-        .padding(.bottom)
-        .padding(.bottom)
-        .padding(.bottom)
-        .padding(.bottom)
     }
 }
-
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
