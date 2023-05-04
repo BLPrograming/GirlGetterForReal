98973545810b934a025cd2d8803f8288f89432b0
BLPrograming <bryan.landsnes@icloud.com>
2023-05-04 13:41:04 -0500

    eeeeeeeeeeee

--- GirlGetter/ContentView.swift
+++ GirlGetter/ContentView.swift
@@ -15,11 +15,26 @@ struct ContentView: View {
             Text("Getter")
                 .foregroundColor(Color(hue: 0.872, saturation: 0.985, brightness: 0.962))
             
+                .padding(.bottom)
+                .padding(.bottom)
+                .padding(.bottom)
+                .padding(.bottom)
+            
+            
+            
+            Button(action: {
+                    print("sign up as girl")
+            }) {
+                Text("Sign Up As A Girl")
+                    .frame(minWidth: 0, maxWidth: 100)
+                  .font(.system(size: 18))
+                  .padding()
+                  .foregroundColor(.black)
+             }
+              .border(Color(hue: 0.872, saturation: 0.985, brightness: 0.962))
+            
         }
-        .padding(.bottom)
-        .padding(.bottom)
-        .padding(.bottom)
-        .padding(.bottom)
+        
         .padding(.bottom)
         .padding(.bottom)
         .padding(.bottom)
