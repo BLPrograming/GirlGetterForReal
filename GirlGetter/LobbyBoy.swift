ee86bacfa12e0ce256481c96f69f671527c1c61e
BLPrograming <bryan.landsnes@icloud.com>
2023-05-12 14:04:53 -0500

    eeeeeeeeeeeeeee er

---
+++ GirlGetter/LobbyBoy.swift
@@ -0,0 +1,20 @@
+//
+//  LobbyBoy.swift
+//  GirlGetter
+//
+//  Created by Bryan C. Landsnes on 5/12/23.
+//
+
+import SwiftUI
+
+struct LobbyBoy: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LobbyBoy_Previews: PreviewProvider {
+    static var previews: some View {
+        LobbyBoy()
+    }
+}
---
+++ GirlGetter/LobbyGirl.swift
@@ -0,0 +1,20 @@
+//
+//  LobbyGirl.swift
+//  GirlGetter
+//
+//  Created by Bryan C. Landsnes on 5/12/23.
+//
+
+import SwiftUI
+
+struct LobbyGirl: View {
+    var body: some View {
+        Text("")
+    }
+}
+
+struct LobbyGirl_Previews: PreviewProvider {
+    static var previews: some View {
+        LobbyGirl()
+    }
+}
